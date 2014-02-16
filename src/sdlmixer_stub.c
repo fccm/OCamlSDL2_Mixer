19d10092190dd6b52fed48c838a411fb5a86a206
Blue Prawn <monnier.florent@gmail.com>
2014-02-16 17:29:37 +0100

    added the names of all the authors in the source code headers

--- src/sdlmixer_stub.c
+++ src/sdlmixer_stub.c
@@ -1,5 +1,6 @@
 /* OCamlSDL2_Mixer - An OCaml interface to the SDL2_mixer library
  Copyright (C) 2013 Florent Monnier
+ Copyright (C) 2014 Piotr Mardziel
  
  This software is provided "AS-IS", without any express or implied warranty.
  In no event will the authors be held liable for any damages arising from
